cf391879b298ef20b9a96567486639473f095a1d
Alexandr <tutbatman@gmail.com>
2016-07-08 22:02:46 +0300

    added loginDialog.html

--- Frontend/app/js/controllers/headerController.js
+++ Frontend/app/js/controllers/headerController.js
@@ -1,8 +1,48 @@
-app.controller('headerController', ['$scope', '$location', function ($scope, $location) {
+app.controller('headerController', ['$scope', '$location', '$mdDialog', '$mdMedia', function ($scope, $location, $mdDialog, $mdMedia) {
 
     $scope.$location = $location;
     $scope.searchText = '';
     $scope.ctrl = {};
 
+    $scope.user = {
+        email: '',
+        password: ''
+    };
+
+    $scope.showLoginDialog = function(ev) {
+        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+
+        $mdDialog.show({
+                controller: DialogController,
+                templateUrl: '../layout/loginDialog.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose:true,
+                fullscreen: useFullScreen
+            })
+            .then(function(answer) {
+                $scope.status = 'You said the information was "' + answer + '".';
+            }, function() {
+                $scope.status = 'You cancelled the dialog.';
+            });
+        $scope.$watch(function() {
+            return $mdMedia('xs') || $mdMedia('sm');
+        }, function(wantsFullScreen) {
+            $scope.customFullscreen = (wantsFullScreen === true);
+        });
+
+    };
+
+    function DialogController($scope, $mdDialog) {
+        $scope.hide = function () {
+            $mdDialog.hide();
+        };
+        $scope.cancel = function () {
+            $mdDialog.cancel();
+        };
+        $scope.answer = function (answer) {
+            $mdDialog.hide(answer);
+        };
+    }
 }]);
 
