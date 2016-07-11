dcb44a4c9c1499974b39ea658550986021e66184
Alexandr <tutbatman@gmail.com>
2016-07-11 19:31:42 +0300

    some improvements in layout

--- Frontend/app/js/services/dictionaryService.js
+++ Frontend/app/js/services/dictionaryService.js
@@ -1,32 +1,27 @@
-/**
- * Created by М on 26.04.2016.
- */
-
 'use strict';
 
-services
-    .factory('dictionaryService', ['$http', function($http){
+services.factory('dictionaryService', ['$http', function ($http) {
 
-        return {
-            getAll: function(uri) {
-                return $http.get(serverUrl + uri)
-                    .success(function(data, status) {
-                        return data;
-                    })
-            },
+    return {
+        getAll: function (uri) {
+            return $http.get(serverUrl + uri)
+                .success(function (data, status) {
+                    return data;
+                })
+        },
 
-            putItem: function(uri, itemForPut) {
-                return $http.put(serverUrl + uri, itemForPut)
-                    .success(function(data, status) {
-                        return data;
-                    })
-            },
+        putItem: function (uri, itemForPut) {
+            return $http.put(serverUrl + uri, itemForPut)
+                .success(function (data, status) {
+                    return data;
+                })
+        },
 
-            deleteItem: function(uri, itemForDelete) {
-                return $http.delete(serverUrl + uri + itemForDelete.id, itemForDelete)
-                    .success(function(data, status) {
-                        return data;
-                    })
-            }
-        };
-    }]);
+        deleteItem: function (uri, itemForDelete) {
+            return $http.delete(serverUrl + uri + itemForDelete.id, itemForDelete)
+                .success(function (data, status) {
+                    return data;
+                })
+        }
+    };
+}]);
